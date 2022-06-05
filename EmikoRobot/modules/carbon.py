6ba9e5939c8a8b61c11d9f8a445bac58ed6665ee
Rizzu Senpai <104607928+RizzuSen@users.noreply.github.com>
2022-06-05 12:50:20 +0700

    Update carbon.py

--- EmikoRobot/modules/carbon.py
+++ EmikoRobot/modules/carbon.py
@@ -24,15 +24,15 @@ async def carbon_func(_, message):
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/11b5922a33de9968cedfe.jpg"
+MEMEK = ""
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Emiko Robot** 
+        caption=f"""✨ **Hey I'm Flox Robot** 
 
-**Owner repo : [sena-ex](https://t.me/excrybaby)**
+**Owner repo : [rizzu は](https://t.me/xflyzu)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -44,9 +44,9 @@ async def repo(_, message):
             [
                 [
                     InlineKeyboardButton(
-                        "Repo", url="https://github.com/kennedy-ex/emikorobot"), 
+                        "Original repo", url="https://github.com/kennedy-ex/emikorobot"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/emikosupport")
+                        "Support", url="https://t.me/Floxsupporttgroup")
                 ]
             ]
         )
